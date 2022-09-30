8f4043e8de7e304b796fbbf02e9d82de32280520
notangelmario <savin@angelmario.eu>
2022-10-01 00:18:36 +0300

    Added dynamic language

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,10 +1,10 @@
 import { locale, loadTranslations } from '$lib/translations';
 import type { LoadEvent } from "@sveltejs/kit"
 
-export const load = async ({ url }: LoadEvent) => {
+export const load = async ({ url, data }: LoadEvent) => {
     const { pathname } = url;
 
-    const defaultLocale = 'en'; // get from cookie, user session, ...
+    const defaultLocale = data?.lang ?? 'en'; // get from cookie, user session, ...
     const initLocale = locale.get() || defaultLocale; // set default if no locale already set
 
     await loadTranslations(initLocale, pathname); // keep this just before the `return`
