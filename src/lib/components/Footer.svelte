8d0f7c3ed9f1c4be6cad5e1a897ca9083d550e54
Savin Angel-Mario <savin@angelmario.eu>
2022-11-15 14:44:08 +0200

    Added footer links

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,24 @@
 <script lang="ts">
 	import Container from "./Container.svelte";
+
+	const links: { href: string; text: string }[] = [
+		{
+			href: "https://blog.fructo.land",
+			text: "Blog",
+		}
+	];
 </script>
 
 <Container>
+	<h3 class="text-2xl">
+		Links
+	</h3>
+	{#each links as link, i}
+		<a class="underline" href="{link.href}" rel="noopener noreferrer">
+			{link.text}
+		</a>
+		{#if i < links.length - 1}
+			&middot;{" "}
+		{/if}
+	{/each}
 </Container>
